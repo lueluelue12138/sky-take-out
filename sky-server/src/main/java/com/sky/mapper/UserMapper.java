16e0fe8ba101e4f3c0559975c2a2a97d39a604c7
Weidong Peng <1792249443@qq.com>
2023-07-24 16:57:01 +0800

    微信小程序实现微信登录、注册，登录拦截器开发

---
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -0,0 +1,29 @@
+package com.sky.mapper;
+
+import com.github.pagehelper.Page;
+import com.sky.annotation.AutoFill;
+import com.sky.dto.EmployeePageQueryDTO;
+import com.sky.entity.Employee;
+import com.sky.entity.User;
+import com.sky.enumeration.OperationType;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+
+    /**
+     * 根据openid查询用户
+     * @param openid
+     * @return
+     */
+    @Select("select * from user where openid = #{openid}")
+    User getByOpenid(String openid);
+
+    /**
+     * 插入用户数据
+     * @param user
+     */
+    void insert(User user);
+}
